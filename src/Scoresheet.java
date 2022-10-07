0834f974429b866fcf37177caca7b8afbf39bb06
wuyuntong <115101272+wuyuntong@users.noreply.github.com>
2022-10-07 11:56:03 +0200

    Update Scoresheet.java

--- src/Scoresheet.java
+++ src/Scoresheet.java
@@ -1,5 +1,5 @@
 public class Scoresheet {
-    public static final int MIN_CROSS = 4;
+    public static final int MIN_CROSS = 5;
     public static final int LOCK_VALUE = 0;
     public static final int PENALTY_VALUE = -5;
     public static final int[][] DEFAULT_NUMBERS = {{2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 0},
