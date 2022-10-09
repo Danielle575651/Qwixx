1e7a212ccc4a1dec22ed9e0c17ebc3c021dcc9af
wuyuntong <115101272+wuyuntong@users.noreply.github.com>
2022-10-09 17:16:03 +0200

    Add files via upload

--- src/DiceGUI.java
+++ src/DiceGUI.java
@@ -3,17 +3,18 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
 
 public class DiceGUI extends JFrame {
     private final JLabel[] picLabels;
-    private JPanel dicePanel;
-    private Dice[] diceSet;
-    private int[] points;
+    private final Dice[] diceSet;
+    private final int[] points;
     private Map<String, Integer> colToNum;
+    private final int FRAME_HEIGHT = 800;
+    private final int FRAMe_WIDTH = 1000;
 
     public DiceGUI() {
         picLabels = new JLabel[6];
-        dicePanel = new JPanel();
         diceSet = new Dice[6];
         addDice(diceSet);
         points = new int[6];
@@ -28,8 +29,8 @@ public class DiceGUI extends JFrame {
         JPanel DicePanel = new JPanel();
         initDicePanel(DicePanel);
 
-        JPanel panel = new JPanel();
-        panel.add(new JButton(new AbstractAction("Roll") {
+        JPanel BPanel = new JPanel();
+        BPanel.add(new JButton(new AbstractAction("Roll") {
             @Override
             public void actionPerformed(ActionEvent e) {
                 DicePanel.removeAll();
@@ -38,27 +39,27 @@ public class DiceGUI extends JFrame {
                 fillDicePanel(DicePanel);
             }
         }));
-        add(panel);
+        add(BPanel, DicePanel, FlowLayout.LEFT);
 
-        setSize(800, 500);
-
+        setSize(FRAMe_WIDTH, FRAME_HEIGHT);
     }
 
     public static void main(String[] args) {
         DiceGUI frame = new DiceGUI();
-        frame.setTitle("Roll Dice");
+        frame.setTitle("Qwixx");
         frame.setVisible(true);
+        frame.setLocationRelativeTo(null);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 
     public void initDicePanel(JPanel panel) {
         setLayout(new FlowLayout());
-        picLabels[0] = new JLabel(new ImageIcon(getClass().getResource("wQ.jpg")));
-        picLabels[1] = new JLabel(new ImageIcon(getClass().getResource("wQ.jpg")));
-        picLabels[2] = new JLabel(new ImageIcon(getClass().getResource("rQ.jpg")));
-        picLabels[3] = new JLabel(new ImageIcon(getClass().getResource("yQ.jpg")));
-        picLabels[4] = new JLabel(new ImageIcon(getClass().getResource("gQ.jpg")));
-        picLabels[5] = new JLabel(new ImageIcon(getClass().getResource("bQ.jpg")));
+        picLabels[0] = new JLabel(new ImageIcon(Objects.requireNonNull(getClass().getResource("wQ.jpg"))));
+        picLabels[1] = new JLabel(new ImageIcon(Objects.requireNonNull(getClass().getResource("wQ.jpg"))));
+        picLabels[2] = new JLabel(new ImageIcon(Objects.requireNonNull(getClass().getResource("rQ.jpg"))));
+        picLabels[3] = new JLabel(new ImageIcon(Objects.requireNonNull(getClass().getResource("yQ.jpg"))));
+        picLabels[4] = new JLabel(new ImageIcon(Objects.requireNonNull(getClass().getResource("gQ.jpg"))));
+        picLabels[5] = new JLabel(new ImageIcon(Objects.requireNonNull(getClass().getResource("bQ.jpg"))));
 
         for (JLabel l : picLabels) {
             panel.add(l);
@@ -74,151 +75,66 @@ public class DiceGUI extends JFrame {
         for (int i = 0; i < points.length; i++) {
             if (i == 0) {
                 switch (points[i]) {
-                    case 0:
-                        picLabels[0] = new JLabel(new ImageIcon(getClass().getResource("wC.jpg")));
-                        break;
-                    case 1:
-                        picLabels[0] = new JLabel(new ImageIcon(getClass().getResource("w1.jpg")));
-                        break;
-                    case 2:
-                        picLabels[0] = new JLabel(new ImageIcon(getClass().getResource("w2.jpg")));
-                        break;
-                    case 3:
-                        picLabels[0] = new JLabel(new ImageIcon(getClass().getResource("w3.jpg")));
-                        break;
-                    case 4:
-                        picLabels[0] = new JLabel(new ImageIcon(getClass().getResource("w4.jpg")));
-                        break;
-                    case 5:
-                        picLabels[0] = new JLabel(new ImageIcon(getClass().getResource("w5.jpg")));
-                        break;
-                    case 6:
-                        picLabels[0] = new JLabel(new ImageIcon(getClass().getResource("w6.jpg")));
-                        break;
+                    case 0 -> picLabels[0] = new JLabel(new ImageIcon(Objects.requireNonNull(getClass().getResource("wC.jpg"))));
+                    case 1 -> picLabels[0] = new JLabel(new ImageIcon(Objects.requireNonNull(getClass().getResource("w1.jpg"))));
+                    case 2 -> picLabels[0] = new JLabel(new ImageIcon(Objects.requireNonNull(getClass().getResource("w2.jpg"))));
+                    case 3 -> picLabels[0] = new JLabel(new ImageIcon(Objects.requireNonNull(getClass().getResource("w3.jpg"))));
+                    case 4 -> picLabels[0] = new JLabel(new ImageIcon(Objects.requireNonNull(getClass().getResource("w4.jpg"))));
+                    case 5 -> picLabels[0] = new JLabel(new ImageIcon(Objects.requireNonNull(getClass().getResource("w5.jpg"))));
+                    case 6 -> picLabels[0] = new JLabel(new ImageIcon(Objects.requireNonNull(getClass().getResource("w6.jpg"))));
                 }
             } else if (i == 1) {
                 switch (points[i]) {
-                    case 0:
-                        picLabels[1] = new JLabel(new ImageIcon(getClass().getResource("wC.jpg")));
-                        break;
-                    case 1:
-                        picLabels[1] = new JLabel(new ImageIcon(getClass().getResource("w1.jpg")));
-                        break;
-                    case 2:
-                        picLabels[1] = new JLabel(new ImageIcon(getClass().getResource("w2.jpg")));
-                        break;
-                    case 3:
-                        picLabels[1] = new JLabel(new ImageIcon(getClass().getResource("w3.jpg")));
-                        break;
-                    case 4:
-                        picLabels[1] = new JLabel(new ImageIcon(getClass().getResource("w4.jpg")));
-                        break;
-                    case 5:
-                        picLabels[1] = new JLabel(new ImageIcon(getClass().getResource("w5.jpg")));
-                        break;
-                    case 6:
-                        picLabels[1] = new JLabel(new ImageIcon(getClass().getResource("w6.jpg")));
-                        break;
+                    case 0 -> picLabels[1] = new JLabel(new ImageIcon(Objects.requireNonNull(getClass().getResource("wC.jpg"))));
+                    case 1 -> picLabels[1] = new JLabel(new ImageIcon(Objects.requireNonNull(getClass().getResource("w1.jpg"))));
+                    case 2 -> picLabels[1] = new JLabel(new ImageIcon(Objects.requireNonNull(getClass().getResource("w2.jpg"))));
+                    case 3 -> picLabels[1] = new JLabel(new ImageIcon(Objects.requireNonNull(getClass().getResource("w3.jpg"))));
+                    case 4 -> picLabels[1] = new JLabel(new ImageIcon(Objects.requireNonNull(getClass().getResource("w4.jpg"))));
+                    case 5 -> picLabels[1] = new JLabel(new ImageIcon(Objects.requireNonNull(getClass().getResource("w5.jpg"))));
+                    case 6 -> picLabels[1] = new JLabel(new ImageIcon(Objects.requireNonNull(getClass().getResource("w6.jpg"))));
                 }
             } else if (i == 2) {
                 switch (points[i]) {
-                    case 0:
-                        picLabels[2] = new JLabel(new ImageIcon(getClass().getResource("rC.jpg")));
-                        break;
-                    case 1:
-                        picLabels[2] = new JLabel(new ImageIcon(getClass().getResource("r1.jpg")));
-                        break;
-                    case 2:
-                        picLabels[2] = new JLabel(new ImageIcon(getClass().getResource("r2.jpg")));
-                        break;
-                    case 3:
-                        picLabels[2] = new JLabel(new ImageIcon(getClass().getResource("r3.jpg")));
-                        break;
-                    case 4:
-                        picLabels[2] = new JLabel(new ImageIcon(getClass().getResource("r4.jpg")));
-                        break;
-                    case 5:
-                        picLabels[2] = new JLabel(new ImageIcon(getClass().getResource("r5.jpg")));
-                        break;
-                    case 6:
-                        picLabels[2] = new JLabel(new ImageIcon(getClass().getResource("r6.jpg")));
-                        break;
+                    case 0 -> picLabels[2] = new JLabel(new ImageIcon(Objects.requireNonNull(getClass().getResource("rC.jpg"))));
+                    case 1 -> picLabels[2] = new JLabel(new ImageIcon(Objects.requireNonNull(getClass().getResource("r1.jpg"))));
+                    case 2 -> picLabels[2] = new JLabel(new ImageIcon(Objects.requireNonNull(getClass().getResource("r2.jpg"))));
+                    case 3 -> picLabels[2] = new JLabel(new ImageIcon(Objects.requireNonNull(getClass().getResource("r3.jpg"))));
+                    case 4 -> picLabels[2] = new JLabel(new ImageIcon(Objects.requireNonNull(getClass().getResource("r4.jpg"))));
+                    case 5 -> picLabels[2] = new JLabel(new ImageIcon(Objects.requireNonNull(getClass().getResource("r5.jpg"))));
+                    case 6 -> picLabels[2] = new JLabel(new ImageIcon(Objects.requireNonNull(getClass().getResource("r6.jpg"))));
                 }
             } else if (i == 3) {
                 switch (points[i]) {
-                    case 0:
-                        picLabels[3] = new JLabel(new ImageIcon(getClass().getResource("yC.jpg")));
-                        break;
-                    case 1:
-                        picLabels[3] = new JLabel(new ImageIcon(getClass().getResource("y1.jpg")));
-                        break;
-                    case 2:
-                        picLabels[3] = new JLabel(new ImageIcon(getClass().getResource("y2.jpg")));
-                        break;
-                    case 3:
-                        picLabels[3] = new JLabel(new ImageIcon(getClass().getResource("y3.jpg")));
-                        break;
-                    case 4:
-                        picLabels[3] = new JLabel(new ImageIcon(getClass().getResource("y4.jpg")));
-                        break;
-                    case 5:
-                        picLabels[3] = new JLabel(new ImageIcon(getClass().getResource("y5.jpg")));
-                        break;
-                    case 6:
-                        picLabels[3] = new JLabel(new ImageIcon(getClass().getResource("y6.jpg")));
-                        break;
+                    case 0 -> picLabels[3] = new JLabel(new ImageIcon(Objects.requireNonNull(getClass().getResource("yC.jpg"))));
+                    case 1 -> picLabels[3] = new JLabel(new ImageIcon(Objects.requireNonNull(getClass().getResource("y1.jpg"))));
+                    case 2 -> picLabels[3] = new JLabel(new ImageIcon(Objects.requireNonNull(getClass().getResource("y2.jpg"))));
+                    case 3 -> picLabels[3] = new JLabel(new ImageIcon(Objects.requireNonNull(getClass().getResource("y3.jpg"))));
+                    case 4 -> picLabels[3] = new JLabel(new ImageIcon(Objects.requireNonNull(getClass().getResource("y4.jpg"))));
+                    case 5 -> picLabels[3] = new JLabel(new ImageIcon(Objects.requireNonNull(getClass().getResource("y5.jpg"))));
+                    case 6 -> picLabels[3] = new JLabel(new ImageIcon(Objects.requireNonNull(getClass().getResource("y6.jpg"))));
                 }
             } else if (i == 4) {
                 switch (points[i]) {
-                    case 0:
-                        picLabels[4] = new JLabel(new ImageIcon(getClass().getResource("gC.jpg")));
-                        break;
-                    case 1:
-                        picLabels[4] = new JLabel(new ImageIcon(getClass().getResource("g1.jpg")));
-                        break;
-                    case 2:
-                        picLabels[4] = new JLabel(new ImageIcon(getClass().getResource("g2.jpg")));
-                        break;
-                    case 3:
-                        picLabels[4] = new JLabel(new ImageIcon(getClass().getResource("g3.jpg")));
-                        break;
-                    case 4:
-                        picLabels[4] = new JLabel(new ImageIcon(getClass().getResource("g4.jpg")));
-                        break;
-                    case 5:
-                        picLabels[4] = new JLabel(new ImageIcon(getClass().getResource("g5.jpg")));
-                        break;
-                    case 6:
-                        picLabels[4] = new JLabel(new ImageIcon(getClass().getResource("g6.jpg")));
-                        break;
+                    case 0 -> picLabels[4] = new JLabel(new ImageIcon(Objects.requireNonNull(getClass().getResource("gC.jpg"))));
+                    case 1 -> picLabels[4] = new JLabel(new ImageIcon(Objects.requireNonNull(getClass().getResource("g1.jpg"))));
+                    case 2 -> picLabels[4] = new JLabel(new ImageIcon(Objects.requireNonNull(getClass().getResource("g2.jpg"))));
+                    case 3 -> picLabels[4] = new JLabel(new ImageIcon(Objects.requireNonNull(getClass().getResource("g3.jpg"))));
+                    case 4 -> picLabels[4] = new JLabel(new ImageIcon(Objects.requireNonNull(getClass().getResource("g4.jpg"))));
+                    case 5 -> picLabels[4] = new JLabel(new ImageIcon(Objects.requireNonNull(getClass().getResource("g5.jpg"))));
+                    case 6 -> picLabels[4] = new JLabel(new ImageIcon(Objects.requireNonNull(getClass().getResource("g6.jpg"))));
                 }
-            } else if (i == 5) {
+            } else {
                 switch (points[i]) {
-                    case 0:
-                        picLabels[5] = new JLabel(new ImageIcon(getClass().getResource("bC.jpg")));
-                        break;
-                    case 1:
-                        picLabels[5] = new JLabel(new ImageIcon(getClass().getResource("b1.jpg")));
-                        break;
-                    case 2:
-                        picLabels[5] = new JLabel(new ImageIcon(getClass().getResource("b2.jpg")));
-                        break;
-                    case 3:
-                        picLabels[5] = new JLabel(new ImageIcon(getClass().getResource("b3.jpg")));
-                        break;
-                    case 4:
-                        picLabels[5] = new JLabel(new ImageIcon(getClass().getResource("b4.jpg")));
-                        break;
-                    case 5:
-                        picLabels[5] = new JLabel(new ImageIcon(getClass().getResource("b5.jpg")));
-                        break;
-                    case 6:
-                        picLabels[5] = new JLabel(new ImageIcon(getClass().getResource("b6.jpg")));
-                        break;
+                    case 0 -> picLabels[5] = new JLabel(new ImageIcon(Objects.requireNonNull(getClass().getResource("bC.jpg"))));
+                    case 1 -> picLabels[5] = new JLabel(new ImageIcon(Objects.requireNonNull(getClass().getResource("b1.jpg"))));
+                    case 2 -> picLabels[5] = new JLabel(new ImageIcon(Objects.requireNonNull(getClass().getResource("b2.jpg"))));
+                    case 3 -> picLabels[5] = new JLabel(new ImageIcon(Objects.requireNonNull(getClass().getResource("b3.jpg"))));
+                    case 4 -> picLabels[5] = new JLabel(new ImageIcon(Objects.requireNonNull(getClass().getResource("b4.jpg"))));
+                    case 5 -> picLabels[5] = new JLabel(new ImageIcon(Objects.requireNonNull(getClass().getResource("b5.jpg"))));
+                    case 6 -> picLabels[5] = new JLabel(new ImageIcon(Objects.requireNonNull(getClass().getResource("b6.jpg"))));
                 }
             }
         }
-
 
         for (JLabel l : picLabels) {
             panel.add(l);
